5684e64de7b70628dc6b5e2ce92939d679bf4d3b
jxltom <jxltom@gmail.com>
2020-03-20 17:01:49 +0800

    Add remote and error frames to example

--- example.py
+++ example.py
@@ -4,24 +4,33 @@ from tests import fake_micropython  # for testing only
 Following SPI drivers are supported, please adjust by your hardware
 from .driver import SPIESP8286 as SPI
 from .driver import SPIESP32 as SPI
+
+
+The CAN driver can be initialized with default baudrate 10MHz
+CAN(SPI(cs=YOUR_SPI_CS_PIN)) or
+CAN(SPI(cs=YOUR_SPI_CS_PIN, baudrate=YOUR_DESIRED_BAUDRATE))
+
+And here is an example to set filter for extended frame with ID 0x12345678
+can.setFilter(RXF.RXF0, True, 0x12345678 | CAN_EFF_FLAG)
+can.setFilterMask(MASK.MASK0, True, 0x1FFFFFFF | CAN_EFF_FLAG)
+can.setFilterMask(MASK.MASK1, True, 0x1FFFFFFF | CAN_EFF_FLAG)
 """
 import time
 
-from driver import CAN, CAN_SPEED, CAN_CLOCK, CANFrame, ERROR
+from driver import (
+    CAN,
+    CAN_CLOCK,
+    CAN_EFF_FLAG,
+    CAN_ERR_FLAG,
+    CAN_RTR_FLAG,
+    CAN_SPEED,
+    ERROR,
+)
 from driver import SPIESP32 as SPI
+from driver import CANFrame
 
 
 def main():
-    """
-    The CAN driver can be initialized with default baudrate 10MHz
-    CAN(SPI(cs=YOUR_SPI_CS_PIN)) or
-    CAN(SPI(cs=YOUR_SPI_CS_PIN, baudrate=YOUR_DESIRED_BAUDRATE))
-
-    And here is an example to set filter for extended frame with ID 0x12345678
-    can.setFilter(RXF.RXF0, True, 0x92345678);
-    can.setFilterMask(MASK.MASK0, True, 0x9FFFFFFF)
-    can.setFilterMask(MASK.MASK1, True, 0x9FFFFFFF)
-    """
     can = CAN(SPI(cs=23))
 
     if can.reset() != ERROR.ERROR_OK:
@@ -34,7 +43,7 @@ def main():
         print("Can not set normal mode for MCP2515")
         return
 
-    end_time = time.ticks_add(time.ticks_ms(), 1000)
+    end_time, n = time.ticks_add(time.ticks_ms(), 1000), -1
     while True:
         # Read message
         error, iframe = can.readMessage()
@@ -44,16 +53,19 @@ def main():
         # Send message every second
         if time.ticks_diff(time.ticks_ms(), end_time) >= 0:
             end_time = time.ticks_add(time.ticks_ms(), 1000)
+            n += 1
+            n %= 4
 
-            oframe = CANFrame(
-                can_id=0x7FF, data=b"\x12\x34\x56\x78\x9A\xBC\xDE\xF0"
-            )  # standard frame
-            oframe = CANFrame(
-                can_id=0x92345678, data=b"\x12\x34\x56\x78\x9A\xBC\xDE\xF0"
-            )  # extended frame
-            error = can.sendMessage(oframe)
+            data = b"\x12\x34\x56\x78\x9A\xBC\xDE\xF0"
+            standard_frame = CANFrame(can_id=0x7FF, data=data)
+            extended_frame = CANFrame(can_id=0x12345678 | CAN_EFF_FLAG, data=data)
+            remote_frame = CANFrame(can_id=0x7FF | CAN_RTR_FLAG)
+            error_frame = CANFrame(can_id=0x7FF | CAN_ERR_FLAG, data=data)
+            frames = [standard_frame, extended_frame, remote_frame, error_frame]
+
+            error = can.sendMessage(frames[n])
             if error == ERROR.ERROR_OK:
-                print("TX  {}".format(oframe))
+                print("TX  {}".format(frames[n]))
             else:
                 print("TX failed with error code {}".format(error))
 
