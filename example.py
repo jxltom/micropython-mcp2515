983f4e7e3f3032e8f658773a53710903b580a45f
jxltom <jxltom@gmail.com>
2020-03-27 13:37:16 +0800

    Update example

--- example.py
+++ example.py
@@ -31,8 +31,10 @@ from driver import CANFrame
 
 
 def main():
+    # Initialization
     can = CAN(SPI(cs=23))
 
+    # Configuration
     if can.reset() != ERROR.ERROR_OK:
         print("Can not reset for MCP2515")
         return
@@ -43,25 +45,38 @@ def main():
         print("Can not set normal mode for MCP2515")
         return
 
+    # Prepare frames
+    data = b"\x12\x34\x56\x78\x9A\xBC\xDE\xF0"
+    sff_frame = CANFrame(canid=0x7FF, data=data)
+    sff_none_data_frame = CANFrame(canid=0x7FF)
+    err_frame = CANFrame(canid=0x7FF | CAN_ERR_FLAG, data=data)
+    eff_frame = CANFrame(canid=0x12345678 | CAN_EFF_FLAG, data=data)
+    eff_none_data_frame = CANFrame(canid=0x12345678 | CAN_EFF_FLAG)
+    rtr_frame = CANFrame(canid=0x7FF | CAN_RTR_FLAG)
+    rtr_with_eid_frame = CANFrame(canid=0x12345678 | CAN_RTR_FLAG | CAN_EFF_FLAG)
+    rtr_with_data_frame = CANFrame(canid=0x7FF | CAN_RTR_FLAG, data=data)
+    frames = [
+        sff_frame,
+        sff_none_data_frame,
+        err_frame,
+        eff_frame,
+        eff_none_data_frame,
+        rtr_frame,
+        rtr_with_eid_frame,
+        rtr_with_data_frame,
+    ]
+
+    # Read all the time and send message in each second
     end_time, n = time.ticks_add(time.ticks_ms(), 1000), -1
     while True:
-        # Read message
         error, iframe = can.readMessage()
         if error == ERROR.ERROR_OK:
             print("RX  {}".format(iframe))
 
-        # Send message every second
         if time.ticks_diff(time.ticks_ms(), end_time) >= 0:
             end_time = time.ticks_add(time.ticks_ms(), 1000)
             n += 1
-            n %= 4
-
-            data = b"\x12\x34\x56\x78\x9A\xBC\xDE\xF0"
-            standard_frame = CANFrame(canid=0x7FF, data=data)
-            extended_frame = CANFrame(canid=0x12345678 | CAN_EFF_FLAG, data=data)
-            remote_frame = CANFrame(canid=0x7FF | CAN_RTR_FLAG)
-            error_frame = CANFrame(canid=0x7FF | CAN_ERR_FLAG, data=data)
-            frames = [standard_frame, extended_frame, remote_frame, error_frame]
+            n %= len(frames)
 
             error = can.sendMessage(frames[n])
             if error == ERROR.ERROR_OK:
